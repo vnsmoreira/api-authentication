6bb094fd627052047a539a47e6814845dc81c7ed
Vinícius <vnsmoreira.me@gmail.com>
2022-03-16 05:47:26 -0300

    create: user model

---
+++ app/models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const bcript = require('bcryptjs');
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.pre('save', async function (next) {
+  const hash = await bcript.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
