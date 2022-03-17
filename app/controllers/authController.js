ff864a17ae7bf64e8569fdf595e472f8024c0cb0
Vinícius <vnsmoreira.me@gmail.com>
2022-03-16 23:53:00 -0300

    improve: remove password (sensible info) from response

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -10,6 +10,8 @@ const register_POST = async (req, res) => {
 
     const user = await User.create(req.body);
 
+    user.password = undefined;
+
     return res.send(user);
   } catch (error) {
     return res.status(400).send({ error: 'Registration failed' });
