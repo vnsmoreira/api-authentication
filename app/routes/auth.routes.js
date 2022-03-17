4509ee88fd15b75953a03d2e70f99548686a6d1b
Vinícius <vnsmoreira.me@gmail.com>
2022-03-17 04:38:35 -0300

    update: create '/reset_password' route

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/register', authController.register_POST);
 router.post('/authenticate', authController.authenticate_POST);
 router.post('/forgot_password', authController.forgot_password_POST);
+router.post('/reset_password', authController.reset_password_POST);
 
 module.exports = app => app.use('/auth', router);
