c81a5ffff9883adae255664967aed79c24361075
Vinícius <vnsmoreira.me@gmail.com>
2022-03-17 04:39:26 -0300

    update: create reset_password auth controller

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -92,4 +92,34 @@ const forgot_password_POST = async (req, res) => {
   }
 };
 
-module.exports = { register_POST, authenticate_POST, forgot_password_POST };
+const reset_password_POST = async (req, res) => {
+  const { email, token, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).select('+passwordResetToken passwordResetExpires');
+
+    if (!user) {
+      return res.status(400).send({ error: 'User not found' });
+    }
+
+    if (token !== user.passwordResetToken) {
+      return res.status(400).send({ error: 'Token invalid' });
+    }
+
+    const now = new Date();
+
+    if (now > user.passwordResetExpires) {
+      return res.status(400).send({ error: 'Token expired, generate a new one' });
+    }
+
+    user.password = password;
+
+    await user.save();
+
+    res.send();
+  } catch (error) {
+    res.status(400).send({ error: 'Cannot reset password, try again' });
+  }
+};
+
+module.exports = { register_POST, authenticate_POST, forgot_password_POST, reset_password_POST };
