24354d66b3562c5f8f8dc63471505ba084e5b918
Vinícius <vnsmoreira.me@gmail.com>
2022-03-17 00:06:49 -0300

    create: /authenticate route

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 const router = Router();
 
 router.post('/register', authController.register_POST);
+router.post('/authenticate', authController.authenticate_POST);
 
 module.exports = router;
