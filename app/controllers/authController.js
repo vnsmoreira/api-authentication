9620a15ebbc41414696cbb97a8f96a53b38bb0d2
Vinícius <vnsmoreira.me@gmail.com>
2022-03-16 23:51:55 -0300

    improve: verify if user already exists and return error

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -2,6 +2,12 @@ const User = require('../models/user');
 
 const register_POST = async (req, res) => {
   try {
+    const { email } = req.body;
+
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: 'User already exists' });
+    }
+
     const user = await User.create(req.body);
 
     return res.send(user);
