c661b982e46e95979f944daa76fd34199396ad95
Vinícius <vnsmoreira.me@gmail.com>
2022-03-18 23:13:53 -0300

    update: define '/project' controllers

--- app/controllers/projectController.js
+++ app/controllers/projectController.js
@@ -1,7 +1,22 @@
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const controller = {};
 
-controller.project_GET = (req, res) => {
-  res.json({ ok: true, user: req.userId });
+controller.listProjects_GET = async (req, res) => {
+  res.json({ user: req.userId });
+};
+controller.showProject_GET = async (req, res) => {
+  res.json({ user: req.userId });
+};
+controller.createProject_POST = async (req, res) => {
+  res.json({ user: req.userId });
+};
+controller.updateProject_PUT = async (req, res) => {
+  res.json({ user: req.userId });
+};
+controller.deleteProject_DELETE = async (req, res) => {
+  res.json({ user: req.userId });
 };
 
 module.exports = controller;
