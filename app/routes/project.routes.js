03f5f29d82513ec9b27d4a652a32ef87d45f3fa8
Vinícius <vnsmoreira.me@gmail.com>
2022-03-18 23:13:19 -0300

    update: define '/project' routes

--- app/routes/project.routes.js
+++ app/routes/project.routes.js
@@ -6,6 +6,10 @@ const router = Router();
 
 router.use(authMiddleware);
 
-router.get('/', projectController.project_GET);
+router.get('/', projectController.listProjects_GET);
+router.get('/:projectId', projectController.showProject_GET);
+router.post('/', projectController.createProject_POST);
+router.put('/:projectId', projectController.updateProject_PUT);
+router.delete('/:projectId', projectController.deleteProject_DELETE);
 
 module.exports = app => app.use('/project', router);
